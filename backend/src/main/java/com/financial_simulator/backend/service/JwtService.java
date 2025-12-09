5e3d7627fe2fa289d6cc72dc7be49636b0964882
CodeWithFlexicon <Felixx.Chenn@gmail.com>
2025-12-09 12:59:53 -0500

    chore: set JWT secret from env file

--- backend/src/main/java/com/financial_simulator/backend/service/JwtService.java
+++ backend/src/main/java/com/financial_simulator/backend/service/JwtService.java
@@ -3,25 +3,29 @@ package com.financial_simulator.backend.service;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.util.Date;
 
 @Service
 public class JwtService {
-    private final String SECRET = "somekeythatwillbereplaced";
-    private final long EXPIRATION = 1000 * 60 * 60 * 24;
+    private final String secretKey;
+
+    public JwtService(@Value("${JWT_SECRET}") String secretKey) {
+        this.secretKey = secretKey;
+    }
 
     // Use SHA256 security for tokens
     public String generateToken(String email) {
         return JWT.create()
                 .withSubject(email)
                 .withIssuedAt(new Date())
-                .withExpiresAt(new Date(System.currentTimeMillis() + EXPIRATION))
-                .sign(Algorithm.HMAC256(SECRET));
+                .withExpiresAt(new Date(System.currentTimeMillis() + 1000 * 60 * 60)) // Change to an hour
+                .sign(Algorithm.HMAC256(secretKey));
     }
 
     public String extractEmail(String token) {
-        return JWT.require(Algorithm.HMAC256(SECRET))
+        return JWT.require(Algorithm.HMAC256(secretKey))
                 .build()
                 .verify(token)
                 .getSubject();
